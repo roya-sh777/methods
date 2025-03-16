2ece9194d367150a776d74c4a80253925eeab2fc
Roya <royash-bb104@code.edu.az>
2025-03-16 11:32:14 -0700

    second

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static double Area(double radius)
+    {
+        return Math.PI * radius * radius;
+    }
+    static void Main()
+    {
+        double radius = 3;
+        Console.WriteLine(Area(radius));
+    }
+}
